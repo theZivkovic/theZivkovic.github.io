6d23f5db9bf6871ab72a0269952f366c8f1e2d9c
Dejan <dejan.zivkovic90@gmail.com>
2017-02-13 18:07:58 +0100

    Emptied the repo

--- javascripts/libs/babylon-dist/preview release/serializers/babylon.objSerializer.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../../../dist/preview release/babylon.d.ts" />
-declare module BABYLON {
-    class OBJExport {
-        static OBJ(mesh: Mesh, materials?: boolean, matlibname?: string): string;
-        static MTL(mesh: Mesh): string;
-    }
-}
