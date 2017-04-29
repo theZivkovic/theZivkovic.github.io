b77670caba26b2d88bc7c924d7ca11922cf06992
Dejan <dejan.zivkovic90@gmail.com>
2017-04-29 12:05:31 +0200

    Simple scene project added

---
+++ simple-scene/javascripts/app.js
@@ -0,0 +1,65 @@
+ // Set the scene size.
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+
+// Set some camera attributes.
+const VIEW_ANGLE = 45;
+const ASPECT = WIDTH / HEIGHT;
+const NEAR = 0.1;
+const FAR = 10000;
+
+const sceneObjects = [];
+
+// Get the DOM element to attach to
+const container = document.querySelector('#container');
+
+// Create a WebGL renderer, camera
+// and a scene
+const renderer = new THREE.WebGLRenderer();
+const camera =
+    new THREE.PerspectiveCamera(
+        VIEW_ANGLE,
+        ASPECT,
+        NEAR,
+        FAR
+    );
+camera.position.z = 500;
+
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+const scene = new THREE.Scene();
+
+// Add the camera to the scene.
+scene.add(camera);
+
+// Start the renderer.
+renderer.setSize(WIDTH, HEIGHT);
+
+// Attach the renderer-supplied
+// DOM element.
+container.appendChild(renderer.domElement);
+
+// create a point light
+const pointLight = new THREE.PointLight(0xFFFFFF);
+
+// set its position
+pointLight.position.x = 10;
+pointLight.position.y = 50;
+pointLight.position.z = 130;
+
+let mesh1 = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10), new THREE.MeshBasicMaterial({color: 0xFF0000}));
+scene.add(mesh1);
+
+// add to the scene
+scene.add(pointLight);
+
+function update () {
+  // Draw!
+  renderer.render(scene, camera);
+
+  // Schedule the next frame.
+  requestAnimationFrame(update);
+}
+
+// Schedule the first frame.
+requestAnimationFrame(update);
